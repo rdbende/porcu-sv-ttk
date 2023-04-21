2a4846226e805a9f092b9f9af0a96b6c1411fcb0
rdbende <rdbende@proton.me>
2023-04-21 22:51:53 +0200

    Add plugin thingy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # porcu-sv-ttk
 Plugin to use the Sun Valley ttk theme in Porcupine
+
+## Installation
+Copy `sun_valley_ttk.py` into Porcupine's extra plugins directory.
---
+++ sun_valley_ttk.py
@@ -0,0 +1,33 @@
+"""Use Sun Valley ttk theme in Porcupine"""
+from pathlib import Path
+
+from porcupine import get_main_window, get_tab_manager
+from porcupine import settings
+from porcupine.settings import global_settings
+
+
+import sv_ttk
+
+def set_theme(theme) -> None:
+    main_window = get_main_window()
+    sv_ttk.set_theme(theme.lower())
+    main_window.option_add("*Text.font", "TkFixedFont")
+    main_window.option_add("*Text.highlightThickness", "0")
+    main_window.option_add("*Text.borderWidth", "1")
+    main_window.option_add("*Text.relief", "solid")
+    main_window.option_add("*Combobox.state", "readonly")
+
+
+
+def setup() -> None:
+    main_window = get_main_window()
+
+    global_settings.add_option("sv_theme", "Dark")
+    settings.add_combobox("sv_theme", "UI theme", values=["Dark", "Light"], state="readonly")
+    set_theme(global_settings.get("sv_theme", str))
+
+    get_tab_manager().bind(
+        "<<GlobalSettingChanged:sv_theme>>",
+        lambda event: set_theme(global_settings.get("sv_theme", str)) or print("semmiség"),
+        add=True,
+    )
